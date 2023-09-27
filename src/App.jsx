11bcc3e50e46308f796594bb36d6db10febfc864
Daffa SH <shidqidaffa@gmail.com>
2023-09-27 14:03:54 +0700

    feat : create form ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ILDesktop from "./assets/illustration-sign-up-desktop.svg";
+import ILMobile from "./assets/illustration-sign-up-mobile.svg";
+import IconList from "./assets/icon-list.svg";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const listText = [
+        { no: 1, text: "Product discovery and building what matters" },
+        { no: 2, text: "Measuring to ensure updates are a success" },
+        { no: 3, text: "And much more!" },
+    ];
 
-  return (
-    <>
-      <div className='flex justify-center'>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='mt-3'>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const [windowWidth, setWindowWidth] = useState(0);
+    const resizeWindow = () => {
+        setWindowWidth(window.innerWidth);
+    };
 
-export default App
+    useEffect(() => {
+        resizeWindow();
+        window.addEventListener("resize", resizeWindow);
+        return () => window.removeEventListener("resize", resizeWindow);
+    }, [windowWidth]);
+
+    return (
+        <div className="h-screen md:flex justify-center items-center">
+            <div className="card bg-white h-screen md:h-fit shadow-xl">
+                <div className="card-body flex flex-row md:flex-nowrap p-0 md:w-full md:p-10 flex-wrap-reverse justify-center items-between md:items-center gap-5">
+                    <div className="px-6">
+                        <h1 className="card-title text-4xl md:text-5xl font-bold text-grey-dark-slate">
+                            Stay updated!{" "}
+                        </h1>
+                        <p className=" font-medium text-grey-dark-slate py-4">
+                            Join 60,000+ product managers receiving monthly {windowWidth >= 768 && <br />}
+                            updates on:
+                        </p>
+                        {listText.map((item) => (
+                            <div key={item.no} className="flex flex-row gap-2 py-1">
+                                <img src={IconList} alt="Album" />
+                                <p className="text-grey-dark-slate font-semibold pl-1"> {item.text}</p>
+                            </div>
+                        ))}
+
+                        <div className="form-control w-full mt-5">
+                            <label className="label">
+                                <span className="label-text text-grey-dark-slate font-semibold">Email Address</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="email@company.com"
+                                className="input input-bordered w-full bg-white text-grey-dark-slate border-grey focus:border-grey-dark-slate focus:outline-none mb-5"
+                            />
+                            <button className="btn btn-primary bg-grey-dark-slate border-grey-dark-slate hover:bg-grey-charcoal hover:border-grey-dark-slate  text-white">
+                                Subscribe to monthly newsletter
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-fit">
+                        <img src={windowWidth <= 768 ? ILMobile : ILDesktop} alt="Album" className="w-full" />
+                    </div>
+                    {console.log(window.innerWidth)}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "grey-dark-slate": "hsl(234, 29%, 20%)",
+                "grey-charcoal": "hsl(235, 18%, 26%)",
+                grey: "hsl(231, 7%, 60%)",
+                white: "hsl(0, 0%, 100%)",
+                tomato: "hsl(4, 100%, 67%)",
+            },
+        },
     },
-    plugins: [],
+    plugins: [require("daisyui")],
 };
